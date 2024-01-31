16c24f7ca935fc87466c2699b3b685cdc858fa1c
fabianommoraes <fabiano.m.moraes@gmail.com>
2024-01-30 23:19:33 -0300

    Fixed dummy db

--- pages/api/battle.js
+++ pages/api/battle.js
@@ -1,24 +1,18 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { promises as fs } from 'fs';
-
-const data = await fs.readFile(process.cwd() + '/pages/api/monsters.json', 'utf8');
-const jsondata = JSON.parse(data)
+import jsondata from "./data";
 
 export default async function handler(req, res) {
-  
-  if (req.method === 'POST') {
-
+  if (req.method === "POST") {
     const { monster1Id, monster2Id } = req.body;
 
     if (!monster1Id || !monster2Id) {
-      return res.status(400).json({ message: 'Missing ID' });
+      return res.status(400).json({ message: "Missing ID" });
     }
 
     const monster1 = jsondata.find((monster) => monster.id === monster1Id);
     const monster2 = jsondata.find((monster) => monster.id === monster2Id);
 
     if (!monster1 || !monster2) {
-      return res.status(400).json({ message: 'Invalid ID' });
+      return res.status(400).json({ message: "Invalid ID" });
     }
 
     const monster1Sum =
--- pages/api/monsters.js
+++ pages/api/monsters.js
@@ -1,18 +1,11 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { promises as fs } from 'fs';
-
-const data = await fs.readFile(process.cwd() + '/pages/api/monsters.json', 'utf8');
-const jsondata = JSON.parse(data)
+import jsondata from "./data";
 
 export default async function handler(req, res) {
-
-  if (req.method === 'GET') {
+  if (req.method === "GET") {
     try {
-      res.status(200).json(jsondata)
-    }
-    catch (error) {
-      console.log(error)
+      res.status(200).json(jsondata);
+    } catch (error) {
+      console.log(error);
     }
   }
-  
 }
